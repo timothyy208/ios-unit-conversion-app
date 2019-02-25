60b4f5289cafdb75bcdaab605d1aac282ef48749
Timothy Yang <timothyy208@gmail.com>
2019-02-25 14:30:10 -0500

    6.5

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var fromUnitsLabel: UILabel!
     @IBOutlet weak var resultsLabel: UILabel!
     @IBOutlet weak var formulaPicker: UIPickerView!
+    @IBOutlet weak var decimalSegment: UISegmentedControl!
     
     var formulaArray = ["miles to kilometers",
                         "kilometers to miles",
@@ -29,37 +30,47 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         formulaPicker.delegate = self
         formulaPicker.delegate = self
+        conversionString = formulaArray[formulaPicker.selectedRow(inComponent: 0)]
 
     }
 
     func calculateConversion(){
-        var outputValue = 0.0
-        if let inputValue = Double(userInput.text!) {
-            switch conversionString {
-            case "miles to kilometers":
-                outputValue = inputValue / 0.62137
-            case "kilometers to miles":
-                outputValue = inputValue * 0.62137
-            case "feet to meters":
-                outputValue = inputValue / 3.2808
-            case "yards to meters":
-                outputValue = inputValue / 1.0936
-            case "meters to feet":
-                outputValue = inputValue * 3.2808
-            case "meters to yards":
-                outputValue = inputValue * 1.0936
-            default:
-                print("sohw alert no conversion string")
-            }
-            resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputValue) \(toUnits)"
-        } else {
+        guard let inputValue = Double(userInput.text!) else {
             print("show alrt here to say the value entered was not a number")
+            return
         }
+        var outputValue = 0.0
+
+        switch conversionString {
+        case "miles to kilometers":
+            outputValue = inputValue / 0.62137
+        case "kilometers to miles":
+            outputValue = inputValue * 0.62137
+        case "feet to meters":
+            outputValue = inputValue / 3.2808
+        case "yards to meters":
+            outputValue = inputValue / 1.0936
+        case "meters to feet":
+            outputValue = inputValue * 3.2808
+        case "meters to yards":
+            outputValue = inputValue * 1.0936
+        default:
+            print("sohw alert no conversion string")
+        }
+        let formatString = (decimalSegment.selectedSegmentIndex < decimalSegment.numberOfSegments-1 ? "%.\(decimalSegment.selectedSegmentIndex+1)f" : "%f")
+        let outputString = String(format: formatString, outputValue)
+        resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputString) \(toUnits)"
+
         
         
     }
     
+    @IBAction func decimalSelected(_ sender: Any) {
+        calculateConversion()
+    }
+    
     @IBAction func convertButtonPressed(_ sender: Any) {
+        calculateConversion()
     }
     
 }
