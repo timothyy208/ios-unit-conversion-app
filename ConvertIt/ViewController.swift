2a40078f5b35be3bcc140c0a1ede7462a9939012
Timothy Yang <timothyy208@gmail.com>
2019-02-24 23:46:16 -0500

    6.2

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var fromUnitsLabel: UILabel!
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var formulaPicker: UIPickerView!
+    
+    var formulaArray = ["miles to kilomters",
+                        "kilometers to miles",
+                        "feet to meters",
+                        "meters to feet",
+                        "meters to feet",
+                        "meters to yards"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        formulaPicker.delegate = self
+        formulaPicker.delegate = self
+
     }
 
+    @IBAction func convertButtonPressed(_ sender: Any) {
+    }
+    
+}
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return formulaArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return formulaArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        fromUnitsLabel.text = formulaArray[row]
+    }
+    
 }
 
